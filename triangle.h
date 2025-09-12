8b1373926b738b8625e2be138b98be58ebf82fdb
Pedro Henrique Holzmeier Schaefer <pedro.schaefer@estudante.uffs.edu.br>
2025-09-12 20:40:18 -0300

    p1

---
+++ triangle.h
@@ -0,0 +1,23 @@
+//
+typedef struct{
+    int a;
+    int b;
+    int c;
+} trngl;
+//
+void setTriangle(trngl *t, int a, int b, int c);
+//
+int getType(trngl t);
+//
+void showTriangle(trngl t);
+//
+int getPerimeter(trngl t);
+//
+double gerArea(trngl t);
+//
+int isSimilar(trngl t1, trngl t2);
+
+
+
+
+
